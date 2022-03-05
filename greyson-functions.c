c2535b045e29e22350c315a87cb74232c3a51c95
Greyson Wilson <gqwilson@cpp.edu>
2022-03-05 15:17:00 -0800

    Mirrored code from Greyson-Wilson-Branch on https://github.com/KennedyJanto2/Travel-Expenses/

--- greyson-functions.c
+++ greyson-functions.c
@@ -3,8 +3,92 @@
 
 typedef enum { false, true } bool;
 
-void helloWorld(){
-    printf("Hello world! This is the initial commit for Project 2 - CS2600 Spring '22.");
+
+float getCarRentals(Trip *trip){
+    return trip->rentalCost;
 }
 
+void setCarRentals(Trip *trip, float cost){
+    if (cost < 0){
+        return;
+    }
+    trip->rentalCost = cost;
+}
 
+int getDaysSpent(Trip *trip){ //Awaiting testing, seems like a hacky way to implement
+    return trip->daysSpent; //(trip->day)/(sizeof(trip->day[0]));
+}
+
+void setDaysSpent(Trip *trip, int days){ //Not sure how to interpret this or whether to do
+    if (days < 0){
+        return;
+    }
+    trip->daysSpent = days;
+}
+
+float getMilesDriven(Trip *trip){
+    float miles = 0;
+    int daysSpent = getDaysSpent(trip);
+    for (int day=0; day < daysSpent; day++){
+        miles += trip->day[day].milesDriven;
+    }
+    return miles;
+}
+
+void setMilesDriven(Day *day, float miles){
+    if (miles < 0){
+        return;
+    }
+    day->milesDriven = miles;
+}
+
+float getParkingCost(Trip *trip){
+    float cost = 0;
+    int daysSpent = getDaysSpent(trip);
+    for (int day=0; day < daysSpent; day++){
+        cost += trip->day[day].parkingCost;
+    }
+    return cost;
+}
+
+void setParkingCost(Day *day, float cost){
+    if (cost < 0){
+        return;
+    }
+    day->parkingCost = cost;
+}
+
+float getTaxiCost(Trip *trip){
+    float cost = 0;
+    int daysSpent = getDaysSpent(trip);
+    for (int day=0; day < daysSpent; day++){
+        cost += trip->day[day].taxiCost;
+    }
+    return cost;
+}
+
+void setTaxiCost(Day *day, float cost){
+    if (cost < 0){
+        return;
+    }
+    day->taxiCost = cost;
+}
+
+void ReimbursableExpenses(Trip *trip){
+    int spent = TotalExpenses(&trip);
+    int allowed = TotalAllowableExpenses(&trip);
+    if (spent > allowed){
+        return spent - allowed;
+    }
+    return 0;
+}
+
+void AmountSaved(Trip *trip){
+    int spent = TotalExpenses(&trip);
+    int allowed = TotalAllowableExpenses(&trip);
+
+    if (spent < allowed){
+        return allowed - spent;
+    }
+    return 0;
+}
--- greyson-functions.h
+++ greyson-functions.h
@@ -1,24 +1,47 @@
 #ifndef GREYSON-FUNCTIONS_H
 #define GREYSON-FUNCTIONS_H
 
-extern const MAXTRIPLENGTH = 30;
+#define MAXTRIPLENGTH 30
 
 typedef struct Day {
     float taxiCost;
     float milesDriven;
     float parkingCost;
-    float mealExpenses;
+    float meals[3]; // 0: Breakfast, 1: Lunch, 2: Dinner
 } Day;
 
 typedef struct Trip {     //Allows use of Trip alone instead of struct Trip
-    Trip day[MAXTRIPLENGTH]; //Perhaps change to dynamically set size of array.
+    Day day[MAXTRIPLENGTH]; //Perhaps change to dynamically set size of array.
     float rentalCost;
     float airfareCost;
     int departTime;
     int arrivalTime;
     float hotelCost;        //Does this belong in day or trip?
     float eventCost;        //Does this belong in day or trip?
+    int daysSpent;
 } Trip;
 
-void helloworld();
+float getCarRentals(Trip *trip);
+
+void setCarRentals(Trip *trip, float cost);
+
+int getDaysSpent(Trip *trip);
+
+void setDaysSpent(Trip *trip, int days);
+
+float getMilesDriven(Trip *trip);
+
+void setMilesDriven(Day *day, float miles);
+
+float getParkingCost(Trip *trip);
+
+void setParkingCost(Day *day, float cost);
+
+float getTaxiCost(Trip *trip);
+
+void setTaxiCost(Day *day, float cost);
+
+float ReimbursableExpenses(Trip *trip);
+
+float AmountSaved(Trip *trip);
 #endif
